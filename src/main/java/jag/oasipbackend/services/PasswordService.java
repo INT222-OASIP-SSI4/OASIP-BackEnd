b5d63770769c9141f87edc3af59cfcdf32d6b0ca
smile1346 <kantapon.tantra@mail.kmutt.ac.th>
2022-08-28 22:15:26 +0700

    [Update] change port

--- src/main/java/jag/oasipbackend/services/PasswordService.java
+++ src/main/java/jag/oasipbackend/services/PasswordService.java
@@ -14,26 +14,10 @@ public class PasswordService {
     }
 
     public String securePassword(String password){
-//        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id, 32, 64);
-//        argon2.hash(10, 65536, 1, password.toCharArray());
         try{
              return argon2.hash(22, 65536, 1, password.toCharArray());
-        }finally {
+        } finally {
             argon2.wipeArray(password.toCharArray());
         }
     }
-
-//    public boolean validatePassword(String hash, String password){
-//        return argon2.verify(hash, password.toCharArray());
-//    }
-
-//    private Argon2 getArgon2Instance() {
-//        Argon2Factory.Argon2Types type = Argon2Factory.Argon2Types.ARGON2d;
-//        if(passwordConfig.getType() == 1){
-//            type = Argon2Factory.Argon2Types.ARGON2i;
-//        }else if(passwordConfig.getType() == 2){
-//            type = Argon2Factory.Argon2Types.ARGON2id;
-//        }
-//        return Argon2Factory.create(type, passwordConfig.getSaltLength(), passwordConfig.getHashLength());
-//    }
 }
