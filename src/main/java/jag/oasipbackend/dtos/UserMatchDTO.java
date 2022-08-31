66641031f63cf676af074ce3b2a5a215b0c59b7c
Jajahz <jajah.asmp@gmail.com>
2022-08-31 19:10:28 +0700

    [UPDATE] validate match function

--- src/main/java/jag/oasipbackend/dtos/UserMatchDTO.java
+++ src/main/java/jag/oasipbackend/dtos/UserMatchDTO.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -15,8 +18,14 @@ public class UserMatchDTO {
 
     private String userName;
 
+    @NotNull(message = "password must not null")
+    @NotBlank(message = "password must not null")
+    @NotEmpty(message = "password must not null")
     private String password;
 
+    @NotNull(message = "userEmail must not null")
+    @NotBlank(message = "userEmail must not null")
+    @NotEmpty(message = "userEmail must not null")
     @Email(message = "userEmail is invalid email, please input correct email form")
     private String userEmail;
 
