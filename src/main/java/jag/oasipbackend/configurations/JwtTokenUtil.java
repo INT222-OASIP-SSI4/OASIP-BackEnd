b9ad24d8fadaa94d33c25133457f171b8d09b151
smile1346 <kantapon.tantra@mail.kmutt.ac.th>
2022-10-05 03:18:35 +0700

    [Update] add role for generate token

--- src/main/java/jag/oasipbackend/configurations/JwtTokenUtil.java
+++ src/main/java/jag/oasipbackend/configurations/JwtTokenUtil.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -12,6 +13,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtTokenUtil implements Serializable {
@@ -53,6 +55,8 @@ public class JwtTokenUtil implements Serializable {
     //generate token for user
     public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
+        final String authorities = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(","));
+        claims.put("Roles", authorities);
         return doGenerateToken(claims, userDetails.getUsername());
     }
 
