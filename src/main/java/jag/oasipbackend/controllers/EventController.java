33926269c66cfd866a8745d7e201c0a9b85c8e1c
smile1346 <kantapon.tantra@mail.kmutt.ac.th>
2022-10-11 23:06:42 +0700

    [Update] edit authen event

--- src/main/java/jag/oasipbackend/controllers/EventController.java
+++ src/main/java/jag/oasipbackend/controllers/EventController.java
@@ -29,7 +29,7 @@ public class EventController {
        return service.findAllEvent(httpServletRequest);
     }
 
-    @GetMapping("/")
+    @GetMapping("/validate")
     public List<EventDTO> getEventForValidate(){
         return service.findAll();
     }
