d2b6cb91e47d8e0cce1ac49e957ecc3ea0e30538
smile1346 <kantapon.tantra@mail.kmutt.ac.th>
2022-12-20 06:13:17 +0700

    [Update] Rebuild project

--- src/main/java/jag/oasipbackend/configurations/JwtTokenUtil.java
+++ src/main/java/jag/oasipbackend/configurations/JwtTokenUtil.java
@@ -88,7 +88,7 @@ public class JwtTokenUtil implements Serializable {
     //3. According to JWS Compact Serialization(https://tools.ietf.org/html/draft-ietf-jose-json-web-signature-41#section-3.1)
     //   compaction of the JWT to a URL-safe string
     private String doGenerateToken(Map<String, Object> claims, String subject) {
-        return Jwts.builder().setClaims(claims).setIssuer("https://intproj21.sit.kmutt.ac.th/or2/").setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+        return Jwts.builder().setClaims(claims).setIssuer("https://intproj21.sit.kmutt.ac.th/ssi4/").setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000)) //set expire กำหนดอายุ token หน่วยมิลลิ
                 .signWith(SignatureAlgorithm.HS512, secret).compact();
     }
--- src/main/java/jag/oasipbackend/controllers/UserController.java
+++ src/main/java/jag/oasipbackend/controllers/UserController.java
@@ -11,7 +11,6 @@ import jag.oasipbackend.responses.ResponseHandler;
 import jag.oasipbackend.services.UserService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
